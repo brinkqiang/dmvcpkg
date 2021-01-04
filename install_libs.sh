f6c7dcf84e1aa3d703aa403bff209d929f366c05
brinkqiang <brink.qiang@gmail.com>
2021-01-04 16:12:50 +0800

    add

--- install_libs.sh
+++ install_libs.sh
@@ -2,6 +2,6 @@
 
 pushd vcpkg
 #arm-uwp,arm-windows,arm64-uwp,arm64-windows,x64-linux,x64-osx,x64-uwp,x64-windows,x64-windows-static
-vcpkg install boost:x64-linux
+./vcpkg install boost:x64-linux
 #vcpkg export boostx64-linux --7zip
 popd
--- install_vcpkg.sh
+++ install_vcpkg.sh
@@ -3,7 +3,7 @@
 git clone --recursive -v https://github.com/Microsoft/vcpkg.git
 pushd vcpkg
 ./bootstrap-vcpkg.sh
-vcpkg integrate remove
-vcpkg integrate install
-vcpkg integrate project
+./vcpkg integrate remove
+./vcpkg integrate install
+./vcpkg integrate project
 popd
