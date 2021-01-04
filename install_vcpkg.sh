2468f2054c3768e2477318e6042a83a577545e67
brinkqiang <brink.qiang@gmail.com>
2021-01-04 16:01:23 +0800

    add

--- install_vcpkg.sh
+++ install_vcpkg.sh
@@ -1,8 +1,7 @@
 git clone --recursive -v https://github.com/Microsoft/vcpkg.git
 pushd vcpkg
-bash bootstrap-vcpkg.sh
+./bootstrap-vcpkg.sh
 vcpkg integrate remove
 vcpkg integrate install
 vcpkg integrate project
-vcpkg integrate powershell
 popd
