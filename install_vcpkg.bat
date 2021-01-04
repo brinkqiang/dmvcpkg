d52607d41d97c544b86a24488652ee64d2cf7870
brinkqiang <brink.qiang@gmail.com>
2021-01-04 18:36:03 +0800

    add

--- install_vcpkg.bat
+++ install_vcpkg.bat
@@ -1,7 +1,7 @@
 set http_proxy=http://127.0.0.1:51080
 set https_proxy=http://127.0.0.1:51080
 
-git clone --recursive -v https://github.com/Microsoft/vcpkg.git
+git clone --recursive -v https://github.com/brinkqiang2cpp/vcpkg.git
 pushd vcpkg
 call bootstrap-vcpkg.bat
 echo vcpkg integrate remove
--- install_vcpkg.sh
+++ install_vcpkg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --recursive -v https://github.com/Microsoft/vcpkg.git
+git clone --recursive -v https://github.com/brinkqiang2cpp/vcpkg.git
 pushd vcpkg
 ./bootstrap-vcpkg.sh
 echo ./vcpkg integrate remove
