f807d966d637e85b9216d32ebec0f144fab2f8b4
brinkqiang <brink.qiang@gmail.com>
2022-05-03 16:03:25 +0800

    add

---
+++ install_qt5.bat
@@ -0,0 +1,10 @@
+set http_proxy=http://127.0.0.1:51080
+set https_proxy=http://127.0.0.1:51080
+
+pushd vcpkg
+::arm-uwp,arm-windows,arm64-uwp,arm64-windows,x64-linux,x64-osx,x64-uwp,x64-windows,x64-windows-static
+vcpkg install qt5:x64-windows-static
+::vcpkg export qt5:x64-windows-static --7zip
+popd
+
+pause
