9205faf21accfae7d2160b6b9c01df41b6496bc5
brinkqiang <brink.qiang@gmail.com>
2022-05-05 03:40:32 +0800

    add

--- sample/dmspawn/test/dmspawnclient/asio-spawn-echo-client.cpp
+++ sample/dmspawn/test/dmspawnclient/asio-spawn-echo-client.cpp
@@ -1,90 +1,90 @@
 ﻿//#include "pch.h"
 #include "asio-spawn-echo-client.h"
-#include "dmlog.hpp"
+#include "dmlog.h"
 
-session_t::session_t(io_context_t &io_context)
-	: socket(io_context)
+session_t::session_t(io_context_t& io_context)
+    : socket(io_context)
 {
 }
 
 session_t::~session_t()
 {
-	LOG_INFO("log");
+    LOG_INFO("log");
 }
 
 void session_t::go()
 {
-	auto self(shared_from_this());
-	asio::spawn(socket.get_executor(),
-		[this, self](yield_context_t yield)
-	{
-		string message;
-		while (std::getline(cin, message))
-		{
-			LOG_INFO("{0}", message);
-			error_code_t ec;
-			auto size = socket.async_write_some(asio::buffer(message), yield[ec]);
+    auto self(shared_from_this());
+    asio::spawn(socket.get_executor(),
+        [this, self](yield_context_t yield)
+    {
+        string message;
+        while (std::getline(cin, message))
+        {
+            LOG_INFO("{0}", message);
+            error_code_t ec;
+            auto size = socket.async_write_some(asio::buffer(message), yield[ec]);
 
-			if (ec)
-			{
-				LOG_INFO("{0}", ec.message());
-			}
-			else
-			{
-				LOG_INFO("async_write_some:{0}", size);
+            if (ec)
+            {
+                LOG_INFO("{0}", ec.message());
+            }
+            else
+            {
+                LOG_INFO("async_write_some:{0}", size);
 
-				vector<char> buff(1024, '\0');
+                vector<char> buff(1024, '\0');
 
-				while (true)
-				{
-					auto read_size = socket.async_read_some(asio::buffer(buff), yield[ec]);
-					if (ec)
-					{
-						LOG_ERROR("{0}", ec.message());
-						exit(1);
-					}
-					else
-					{
-						copy_n(buff.begin(), read_size, back_inserter(buffer));
-						if (read_size < buff.size())
-						{
-							break;
-						}
-					}
-				}
+                while (true)
+                {
+                    auto read_size = socket.async_read_some(asio::buffer(buff), yield[ec]);
+                    if (ec)
+                    {
+                        LOG_ERROR("{0}", ec.message());
+                        exit(1);
+                    }
+                    else
+                    {
+                        copy_n(buff.begin(), read_size, back_inserter(buffer));
+                        if (read_size < buff.size())
+                        {
+                            break;
+                        }
+                    }
+                }
 
-				buffer.push_back('\0');
-				LOG_INFO("async_read_some:{0}", buffer.data());
-				buffer.clear();
-			}
-		}
-	});
+                buffer.push_back('\0');
+                LOG_INFO("async_read_some:{0}", buffer.data());
+                buffer.clear();
+            }
+        }
+    });
 }
 
 int main()
 {
-	io_context_t io_context;
+    io_context_t io_context;
 
-	asio::spawn(io_context,
-		[&io_context](yield_context_t yield)
-	{
-		error_code_t ec;
+    asio::spawn(io_context,
+        [&io_context](yield_context_t yield)
+    {
+        error_code_t ec;
 
-		auto session = make_shared<session_t>(io_context);
-		auto &socket = session->socket;
+        auto session = make_shared<session_t>(io_context);
+        auto& socket = session->socket;
 
-		socket.async_connect(endpoint_t(address_t::from_string("127.0.0.1"), 12500), yield[ec]);
-		if (ec)
-		{
-			LOG_ERROR("{0}", ec.message());
-			exit(1);
-		}
-		else
-		{
-			LOG_INFO("address:{0} port:{1}", session->address, session->port);
-			session->go();
-		}
-	});
+        socket.async_connect(endpoint_t(address_t::from_string("127.0.0.1"), 12500), yield[ec]);
+        if (ec)
+        {
+            LOG_ERROR("{0}", ec.message());
+            exit(1);
+        }
+        else
+        {
+            LOG_INFO("address:{0} port:{1}", session->address, session->port);
+            session->go();
+        }
+    });
 
-	io_context.run();
+    io_context.run();
 }
--- sample/dmspawn/test/dmspawnserver/asio-spawn-echo-server.cpp
+++ sample/dmspawn/test/dmspawnserver/asio-spawn-echo-server.cpp
@@ -1,89 +1,89 @@
 ﻿//#include "pch.h"
 #include "asio-spawn-echo-server.h"
-#include "dmlog.hpp"
-session_t::session_t(io_context_t &io_context)
-	: socket(io_context)
+#include "dmlog.h"
+session_t::session_t(io_context_t& io_context)
+    : socket(io_context)
 {
 }
 
 session_t::~session_t()
 {
-	LOG_INFO("log");
+    LOG_INFO("log");
 }
 
 void echo(shared_ptr<session_t> session, yield_context_t& yield)
 {
-	auto &socket = session->socket;
-	error_code_t ec;
-	vector<char> buffer(1024, '\0');
-	auto size = socket.async_read_some(asio::buffer(buffer), yield[ec]);
-	if (ec)
-	{
-		LOG_INFO("{0}", ec.message());
-		return;
-	}
-	else
-	{
-		LOG_INFO("async_read_some:{0}", size);
-		LOG_INFO("hex:{0}", to_hex(buffer, size));
-		LOG_INFO("{0}", buffer.data());
-		auto write_size = socket.async_write_some(asio::buffer(buffer.data(), size), yield[ec]);
-		if (ec)
-		{
-			LOG_INFO("{0}", ec.message());
-			return;
-		}
-		else
-		{
-			LOG_INFO("async_read_some:{0}", write_size);
-			echo(session, yield);
-		}
-	}
+    auto& socket = session->socket;
+    error_code_t ec;
+    vector<char> buffer(1024, '\0');
+    auto size = socket.async_read_some(asio::buffer(buffer), yield[ec]);
+    if (ec)
+    {
+        LOG_INFO("{0}", ec.message());
+        return;
+    }
+    else
+    {
+        LOG_INFO("async_read_some:{0}", size);
+        LOG_INFO("hex:{0}", to_hex(buffer, size));
+        LOG_INFO("{0}", buffer.data());
+        auto write_size = socket.async_write_some(asio::buffer(buffer.data(), size), yield[ec]);
+        if (ec)
+        {
+            LOG_INFO("{0}", ec.message());
+            return;
+        }
+        else
+        {
+            LOG_INFO("async_read_some:{0}", write_size);
+            echo(session, yield);
+        }
+    }
 }
 
 void session_t::go()
 {
-	auto self(shared_from_this());
-	asio::spawn(socket.get_executor(),
-		[this, self](yield_context_t yield)
-	{
-		echo(self, yield);
-	});
+    auto self(shared_from_this());
+    asio::spawn(socket.get_executor(),
+        [this, self](yield_context_t yield)
+    {
+        echo(self, yield);
+    });
 }
 
 int main()
 {
-	io_context_t io_context;
+    io_context_t io_context;
 
-	acceptor_t acceptor(io_context,
-		endpoint_t(address_t::from_string("0.0.0.0"), 12500));
+    acceptor_t acceptor(io_context,
+        endpoint_t(address_t::from_string("0.0.0.0"), 12500));
 
-	asio::spawn(io_context,
-		[&acceptor, &io_context](yield_context_t yield)
-	{
-		while (true)
-		{
-			error_code_t ec;
-			auto session = make_shared<session_t>(io_context);
-			auto &socket = session->socket;
-			acceptor.async_accept(socket, yield[ec]);
-			if (ec)
-			{
-				LOG_INFO("{0}", ec.message());
-				continue;
-			}
-			else
-			{
-				auto remote_endpoint = socket.remote_endpoint();
-				auto address = remote_endpoint.address().to_string();
-				auto port = remote_endpoint.port();
-				LOG_INFO("address:{0} port:{1}", address, port);
+    asio::spawn(io_context,
+        [&acceptor, &io_context](yield_context_t yield)
+    {
+        while (true)
+        {
+            error_code_t ec;
+            auto session = make_shared<session_t>(io_context);
+            auto& socket = session->socket;
+            acceptor.async_accept(socket, yield[ec]);
+            if (ec)
+            {
+                LOG_INFO("{0}", ec.message());
+                continue;
+            }
+            else
+            {
+                auto remote_endpoint = socket.remote_endpoint();
+                auto address = remote_endpoint.address().to_string();
+                auto port = remote_endpoint.port();
+                LOG_INFO("address:{0} port:{1}", address, port);
 
-				session->go();
-			}
-		}
-	});
+                session->go();
+            }
+        }
+    });
 
-	io_context.run();
-	return 0;
+    io_context.run();
+    return 0;
 }
