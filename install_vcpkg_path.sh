3fe84162741755c7ed111ff4b65e6ec1f37424da
brinkqiang <brink.qiang@gmail.com>
2024-04-24 17:33:46 +0800

    add

--- install_vcpkg_path.sh
+++ install_vcpkg_path.sh
@@ -12,7 +12,8 @@ function add_or_replace_permanent_env_var() {
   # 检查变量是否已经存在于/etc/environment文件中
   if grep -q "${var_name}=" /etc/environment; then
     # 用新值替换变量的旧值
-    sudo sed -i "s/^${var_name}=.*/${var_name}=\"${var_value}\"/" /etc/environment
+    sudo sed -i "s|^${var_name}=.*|${var_name}=\"${var_value}\"|" /etc/environment
+
     echo "永久环境变量 "${var_name}=\"${var_value}\"" 已被替换"
   else
     # 将新的键值对添加到/etc/environment文件中
