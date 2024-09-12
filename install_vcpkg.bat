8b73dcba45029a9c963b6280caf34e2c44c710fb
brinkqiang <brink.qiang@gmail.com>
2024-09-12 10:29:49 +0800

    add

--- install_vcpkg.bat
+++ install_vcpkg.bat
@@ -2,7 +2,7 @@ rem set http_proxy=http://127.0.0.1:51080
 rem set https_proxy=http://127.0.0.1:51080
 
 rem git clone --recursive -v https://github.com/brinkqiang2cpp/vcpkg.git
-git clone --recursive -v https://github.com/Microsoft/vcpkg.git
+git clone --recursive -v git@github.com:microsoft/vcpkg.git
 
 
 pushd vcpkg
